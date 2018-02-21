b2a3c495743dfa84f659468fbec8cc1d4710e7b4
patrickatcw <pj8392@yahoo.com>
2018-02-21 12:35:10 -0500

    created 3 fields, start working on the 3 constructors next

--- src/com/me/VipCustomer.java
+++ src/com/me/VipCustomer.java
@@ -1,4 +1,24 @@
+//Directions
+/*
+1) create a new class VipCustomer
+2) it should have 3 fields; name, credit limit, and email address
+3) create 3 constructors
+- 1st constructor empty should call the constructor with 3 parameters
+with default value
+- 2nd constructor should pass on the 2 values it receives and add a
+default value for the 3rd
+- 3rd constructor should save all fields
+4) create getters only for this using generation of intellij as setter
+won't be needed
+5) confirm it works
+*/
+
 package com.me;
 
-public class VipCustomer {          //step 1
+public class VipCustomer {          //step 1 new class created through com.me
+
+    private String name;            //step 2 3 fields created
+    private double creditLimit;
+    private String emailAddress;
+
 }
